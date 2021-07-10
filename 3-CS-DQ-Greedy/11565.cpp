93b5df129119aaaea4159552c2aac69a2a89a23a
nestor <nestorivanmo@comunidad.unam.mx>
2021-07-09 19:03:02 -0500

    more code

---
+++ 3-CS-DQ-Greedy/11565.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A = 6, B = 6, C = 14;
+    bool sol; int x, y, z;
+    for (x=-22; x <= 22 && !sol; x++) if (x*x <= C)
+        for (y = -100; y <= 100 && !sol; y++) if (y != x && x*x + y*y <= C)
+            for (z = -100; z <= 100 && !sol; z++)
+                if (z != x && z != y && x + y + z == A && x*y*z == B && x*x + y*y + z*z == C) {
+                        printf("%d %d %d\n", x, y, z);
+                        sol = true;
+                    }
+}
---
+++ 3-CS-DQ-Greedy/backtracking_subsets.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+#define MAXSUBSETS 100
+
+void printA(vector<int> a) {
+    printf("{");
+    for (int ai : a)
+        printf(" %d", ai);
+    printf(" }\n");
+}
+
+bool isSolution(int n, vector<int> a, int sols) {
+
+}
+
+vector<int> build_candidates(int first, int n) {
+    vector<int> c;
+    for (int i = first; i <= n; i++)
+        c.push_back(i);
+    return c;
+}
+
+int backtrack(int n, vector<int> a, int sols) {
+    if (isSolution(n, a, sols)) {
+        printA(a);
+        sols += 1;
+    }
+    
+    int first;
+    if (a.size() == 0) 
+        first = 1;
+    else 
+        first = *max_element(a.begin(), a.end()) + 1;
+
+    vector<int> candidates = build_candidates(first, n);
+    for (int c : candidates) {
+        a.push_back(c);
+        sols = backtrack(n, a, sols);
+        a.pop_back();
+    }
+    return sols;
+}
+
+int main() {
+    int n = 3;
+    vector<int> a;
+    int sols = backtrack(n, a, 0);
+    printf("Number of solutions for n=%d : %d\n", n, sols);
+}
