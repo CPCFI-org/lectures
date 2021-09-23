a20575114a1ac43da582c6b66e61d4a3fed43047
nestor <nestorivanmo@comunidad.unam.mx>
2021-09-23 18:19:11 -0500

    UVA 10920 - Spiral Trap solution

--- problemsets/uva/spiral-trap-10920/spiraltrap.cpp
+++ problemsets/uva/spiral-trap-10920/spiraltrap.cpp
@@ -1,172 +1,88 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-#define endl                        '\n'
-#define fastIO()                    cin.tie(0); cout.tie(0);
-#define FO(i, b)                    for (int i = 0; i < (b); i++)
-#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
-#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
-#define TR(v, arr)                  for (auto& (v) : (arr))
-#define debug(x)                    cout << #x << " = "; _debug(x); cout << endl;
-#define si(x)	                    scanf("%d",&x)
-#define sl(x)	                    scanf("%lld",&x)
-#define pi(x)	                    printf("%d\n",x)
-#define pl(x)	                    printf("%lld\n",x)
-#define pb                          push_back
-#define mp                          make_pair
-#define F                           first
-#define S                           second
-#define all(x)                      x.begin(), x.end() 
-#define sz(x)                       (int) x.size()
-#define LB(arr, x)                  lower_bound(all(arr), x)-(arr).begin()
-#define UB(arr, x)                  upper_bound(all(arr), x)-(arr).begin()
+#define fastIO          cin.tie(0); cout.tie(0);
+#define endl            '\n'
+#define FO(i, b)        for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)    for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)   for (int i = (a); i > (b); i--)
+#define TR(v, arr)      for(auto& (v) : (arr))
+#define pint(x)         printf("%d\n", x);
+#define pll(x)          printf("%lld\n", x);
+#define si(x)           scanf("%d", &x);
+#define sl(x)           scanf("%lld", &x);
+#define all(x)          x.begin(), x.end()
+
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
+typedef vector<long long> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
 
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
-
-void setIO(){
+void setIO() {
     string file = __FILE__;
     file = string(file.begin(), file.end()-3);
-    string in_file = file+"in";
-    string out_file = file+"out";
-    freopen(in_file.c_str(), "r", stdin);
-    freopen(out_file.c_str(), "w", stdout);
+    string input_file = file + "in";
+    string output_file = file + "out";
+    freopen(input_file.c_str(), "r", stdin);
+    freopen(output_file.c_str(), "w", stdout);
 }
 
-template <typename T>
-void _debug(T& x){
-    cout << x;
-}
+// -----------------------------------------------------------------------------
+// Here begins our solution
+// -----------------------------------------------------------------------------
 
-template <typename T1, typename T2>
-void _debug(pair<T1, T2>& pair){
-    cout << "{"; _debug(pair.F); cout << ","; _debug(pair.S); cout << "}";  
-}
+int sz, P;
+int line, column;
 
-template <typename T>
-void _debug(vector<T>& vec){
-    int n = sz(vec);
-    if(n == 0){cout << "[ ]"; return;}
-    cout << "[";
-    FO(i, n-1){
-        _debug(vec[i]); cout << " ";
-    }
-    _debug(vec[n-1]); cout << "]";
-}
+void solve() {
+    //printf("%d %d\n", sz, P);
+    int P_right_upper_corner = ceil(sqrt(P));
+    if (P_right_upper_corner % 2 == 0) P_right_upper_corner += 1;
+    int level = P_right_upper_corner;
 
-void _debug(vector<string>& vec){
-    int n = sz(vec);
-    cout << endl;
-    FO(i, n){
-        cout << vec[i] << endl;
-    }
-}
+    int col_center = ceil((float)sz/2), row_center = col_center;
+    int col_level = col_center + (level-1)/2, row_level = col_level;
 
-template <typename T>
-void _debug(vector<vector<T>>& A){
-    int n = sz(A);
-    cout << endl;
-    FO(i, n){
-        _debug(A[i]); cout << endl;
-    }
-}
-
-template <typename T>
-void print(T& x){
-    cout << x << endl;
-    return;
-}
-
-template <typename T>
-void print(vector<T>& vec, int a=0, int b=-1){
-    if(b == -1){b = sz(vec);}
-    if(b == 0){return;}
-    FOR(i, a, b-1){
-        cout << vec[i] << " ";
-    }
-    cout << vec[b-1] << endl;
-    return; 
-}
-
-//------------------------------------------------------------
-
-void solve(int sz, int P){
-
-    // 1. Nivel
-
-    int level = (int)ceil(sqrt(P));
-    
-    if (level % 2 == 0) {
-        level += 1;
-    }
+    //printf("Level value: %d\nCenterC (%d,%d)\nLevelC (%d,%d)\n", level, col_center, row_center, col_level, row_level);
 
     int k = level - 1;
-    int D = sz - level;
+    // upper right corner
+    ll urc = level*level;
+    ll lrc = urc - k;
+    ll llc = lrc - k;
+    ll ulc = llc - k;
 
-    // 2. Esquinas
+    //printf("Corners:\n%lld %lld\n%lld %lld\n", ulc, urc, llc, lrc);
 
-    // esq. sup. der
-    int esd = level*level;
+    // First base case
+    if (sz == 1 || P == 1) { line = row_center; column = col_center; return; }
 
-    // esq sup izq
-    int esi = level*level - 3*k;
-
-    // esq. inf. der
-    int eid = level*level - k;
-
-    // esq inf izq
-    int eii = level*level -2*k;
-
-    // debug(level);
-    // debug(k);
-    // debug(esd);
-    // debug(esi);
-    // debug(eid);
-    // debug(eii);
-
-    // debug(P);
-
-    // 3. Rango
-    int diff;
-    int L, C; //L: Linea, C: columna
-    if (P < esi) { // renglón superior
-        L = level;
-        C = 1 + (esi - P);
-    } else if (P < eii) { // columna izquierda
-        C = 1;
-        L = 1 + (eii - P);
-    } else if (P < eid) { // renglón inferior
-        L = 1;
-        C = 1 + (eid - P);
+    if (ulc > P) {
+        line = row_level;
+        column = (col_level - k) + (ulc - P);
+    } else if (llc > P) {
+        line = (row_level-k) + (llc - P);
+        column = col_level - k;
+    } else if (lrc > P) {
+        line = row_level - k;
+        column = col_level - (lrc - P);
+    } else if (urc > P) {
+        line = row_level - (urc - P);
+        column = col_level;
     } else {
-        C = level;
-        L = 1 + (esd - P);
+        line = row_level;
+        column = col_level;
     }
-
-    printf("Line = %d, column = %d\n", L+D, C);
 
     return;
 }
 
-int main()
-{
-    fastIO();
-    if(getenv("LOCAL")){setIO();}
-    
-    int sz, P;
-    while(scanf("%d %d", &sz, &P), sz>0) {
-        solve(sz, P);
+int main() {
+    if (getenv("LOCAL")) { setIO(); }
+    while (scanf("%d %d", &sz, &P), sz != 0 && P != 0) {
+        solve();
+        printf("Line = %lld, column = %lld.\n", line, column);
     }
-
     return 0;
 }
