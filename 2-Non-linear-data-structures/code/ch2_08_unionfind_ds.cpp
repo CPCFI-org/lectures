ee583465816cad35e467f721549ec53767f94fa5
nestor <nestorivanmo@comunidad.unam.mx>
2021-09-27 22:32:00 -0500

    union find code

--- 2-Non-linear-data-structures/code/ch2_08_unionfind_ds.cpp
+++ 2-Non-linear-data-structures/code/ch2_08_unionfind_ds.cpp
@@ -6,24 +6,43 @@ typedef vector<int> vi;
 
 // Union-Find Disjoint Sets Library written in OOP manner, using both path compression and union by rank heuristics
 class UnionFind {                                              // OOP style
-private:
-  vi p, rank, setSize;                       // remember: vi is vector<int>
-  int numSets;
-public:
-  UnionFind(int N) {
-    setSize.assign(N, 1); numSets = N; rank.assign(N, 0);
-    p.assign(N, 0); for (int i = 0; i < N; i++) p[i] = i; }
-  int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
-  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-  void unionSet(int i, int j) { 
-    if (!isSameSet(i, j)) { numSets--; 
-    int x = findSet(i), y = findSet(j);
-    // rank is used to keep the tree short
-    if (rank[x] > rank[y]) { p[y] = x; setSize[x] += setSize[y]; }
-    else                   { p[x] = y; setSize[y] += setSize[x];
-                             if (rank[x] == rank[y]) rank[y]++; } } }
-  int numDisjointSets() { return numSets; }
-  int sizeOfSet(int i) { return setSize[findSet(i)]; }
+    private:
+    vi p, rank, setSize;                       // remember: vi is vector<int>
+    int numSets;
+    public:
+    UnionFind(int N) {
+        setSize.assign(N, 1); 
+        numSets = N; 
+
+        rank.assign(N, 0);
+        p.assign(N, 0);
+
+        for (int i = 0; i < N; i++) 
+        p[i] = i; 
+    }
+    
+    int findSet(int i) { 
+        return (p[i] == i) ? i : (p[i] = findSet(p[i])); 
+    }
+
+    bool isSameSet(int i, int j) { 
+        return findSet(i) == findSet(j); 
+    }
+
+    void unionSet(int i, int j) { 
+        int x = findSet(i), y = findSet(j);
+        if (!isSameSet(i, j)) { 
+            numSets--; 
+            // rank is used to keep the tree short
+            if (rank[x] > rank[y]) { p[y] = x; setSize[x] += setSize[y]; }
+        } else { 
+            p[x] = y; setSize[y] += setSize[x];
+            if (rank[x] == rank[y]) rank[y]++; 
+        } 
+    }
+
+    int numDisjointSets() { return numSets; }
+    int sizeOfSet(int i) { return setSize[findSet(i)]; }
 };
 
 int main() {
