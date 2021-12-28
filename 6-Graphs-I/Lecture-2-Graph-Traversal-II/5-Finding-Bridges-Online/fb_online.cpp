9a50a74e77b3742808f011b2030a3b742e580265
nestor <nestorivanmo@comunidad.unam.mx>
2021-12-28 13:25:49 -0600

    finding bridges online

---
+++ 6-Graphs-I/Lecture-2-Graph-Traversal-II/5-Finding-Bridges-Online/fb_online.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x)                    cout << #x << " = "; _debug(x); cout << endl;
+
+void setIO(){
+    string file = __FILE__;
+    file = string(file.begin(), file.end()-3);
+    string in_file = file+"in";
+    string out_file = file+"out";
+    freopen(in_file.c_str(), "r",  stdin);
+    freopen(out_file.c_str(), "w",  stdout);
+}
+
+void Clean() {
+    printf("\n==================\n\n");
+}
+
+template <typename T>
+void _debug(T& x){
+    cout << x;
+}
+
+template <typename T>
+void _debug(vector<T>& vec){
+    int n = vec.size();
+    if(n == 0){cout << "[ ]"; return;}
+    cout << "[";
+    for (int i = 0; i < n-1; i++) {
+        _debug(vec[i]); cout << " ";
+    }
+    _debug(vec[n-1]); cout << "]";
+}
+
+
+// --------------------------------------------------------------
+// Start coding here: Finding Bridges Online - DSU data structure
+// --------------------------------------------------------------
+
+int n, m;
+vector<int> par, dsu_2ecc, dsu_cc, dsu_cc_size, last_visit;
+int bridges, lca_iteration;
+
+void init(int n) {
+    par.resize(n);
+    dsu_2ecc.resize(n);
+    dsu_cc.resize(n);
+    dsu_cc_size.resize(n);
+    lca_iteration = 0;
+    last_visit.assign(n, 0);
+    for (int i=0; i<n; ++i) {
+        dsu_2ecc[i] = i;
+        dsu_cc[i] = i;
+        dsu_cc_size[i] = 1;
+        par[i] = -1;
+    }
+    bridges = 0;
+}
+
+int find_2ecc(int v) {
+    if (v == -1)
+        return -1;
+    return dsu_2ecc[v] == v ? v : dsu_2ecc[v] = find_2ecc(dsu_2ecc[v]);
+}
+
+int find_cc(int v) {
+    v = find_2ecc(v);
+    return dsu_cc[v] == v ? v : dsu_cc[v] = find_cc(dsu_cc[v]);
+}
+
+void make_root(int v) {
+    v = find_2ecc(v);
+    int root = v;
+    int child = -1;
+    while (v != -1) {
+        int p = find_2ecc(par[v]);
+        par[v] = child;
+        dsu_cc[v] = root;
+        child = v;
+        v = p;
+    }
+    dsu_cc_size[root] = dsu_cc_size[child];
+}
+
+void merge_path (int a, int b) {
+    ++lca_iteration;
+    vector<int> path_a, path_b;
+    int lca = -1;
+    while (lca == -1) {
+        if (a != -1) {
+            a = find_2ecc(a);
+            path_a.push_back(a);
+            if (last_visit[a] == lca_iteration){
+                lca = a;
+                break;
+                }
+            last_visit[a] = lca_iteration;
+            a = par[a];
+        }
+        if (b != -1) {
+            b = find_2ecc(b);
+            path_b.push_back(b);
+            if (last_visit[b] == lca_iteration){
+                lca = b;
+                break;
+                }
+            last_visit[b] = lca_iteration;
+            b = par[b];
+        }
+    }
+    for (int v : path_a) {
+        dsu_2ecc[v] = lca;
+        if (v == lca)
+            break;
+        --bridges;
+    }
+    for (int v : path_b) {
+        dsu_2ecc[v] = lca;
+        if (v == lca)
+            break;
+        --bridges;
+    }
+}
+
+void add_edge(int a, int b) {
+    a = find_2ecc(a);
+    b = find_2ecc(b);
+    if (a == b)
+        return;
+
+    int ca = find_cc(a);
+    int cb = find_cc(b);
+
+    if (ca != cb) {
+        ++bridges;
+        if (dsu_cc_size[ca] > dsu_cc_size[cb]) {
+            swap(a, b);
+            swap(ca, cb);
+        }
+        make_root(a);
+        par[a] = dsu_cc[a] = b;
+        dsu_cc_size[cb] += dsu_cc_size[a];
+    } else {
+        merge_path(a, b);
+    }
+}
+
+int main() {
+    if (getenv("LOCAL")) { setIO(); }
+    printf("Finding Bridges Online\n");
+    cin >> n >> m;
+    init(n);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        printf("Adding edge (%d,%d)\n", a, b);
+        add_edge(a,b);
+        printf("\tNumber of bridges: %d\n", bridges);
+    }
+    Clean();
+    return 0;
+}
