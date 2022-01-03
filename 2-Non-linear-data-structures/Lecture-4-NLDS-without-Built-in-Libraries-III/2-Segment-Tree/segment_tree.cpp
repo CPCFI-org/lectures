c1ead495fed1bc8338ed53ad385f2d30e5eaafed
nestor <nestorivanmo@comunidad.unam.mx>
2022-01-02 22:55:16 -0600

    segment tree

--- 2-Non-linear-data-structures/Lecture-4-NLDS-without-Built-in-Libraries-III/2-Segment-Tree/segment_tree.cpp
+++ 2-Non-linear-data-structures/Lecture-4-NLDS-without-Built-in-Libraries-III/2-Segment-Tree/segment_tree.cpp
@@ -33,19 +33,40 @@ void _debug(vector<T>& vec){
 }
 
 
-// ---------------------------------------------
-// Start coding here: Segment Tree
-// ---------------------------------------------
+// --------------------------------------------------
+// Start coding here: Segment Tree for sum queries
+// Reference: https://codeforces.com/blog/entry/18051
+// --------------------------------------------------
 
-#define MAXN 4
-int n, t[4*MAXN];
+#define INF 1e5
+const int N = 1e5;
+int n;
+int t[2 * N];
 
+void build() {
+    for (int i = n - 1; i > 0; --i)  
+        t[i] = t[i<<1] + t[i<<1 | 1];
+}
 
+void modify(int p, int value) {  // set value at position p
+    for (t[p+=n] = value; p > 1; p >>= 1) 
+        t[p>>1] = t[p] + t[p^1];
+}
+
+int query(int l, int r) {  // sum on interval [l, r)
+    int res = 0;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if (l&1) res += t[l++]; // least significant bit of l is 1? => l is odd?
+        if (r&1) res += t[--r]; // least significant bit of r is 1? => r is odd?
+    }
+    return res;
+}
 
 int main() {
-    if (getenv("LOCAL")) { setIO(); }
-    printf("\n");
-    
-    Clean();
+    if (getenv("LOCAL")) setIO();
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+    build();
+    printf("%d\n", query(3, 11)); // [l,r)
     return 0;
 }
