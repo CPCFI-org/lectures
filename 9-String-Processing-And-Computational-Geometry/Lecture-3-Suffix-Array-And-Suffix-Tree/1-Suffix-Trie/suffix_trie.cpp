1288c5d00f33b5850aef05036402bdaa2f54e0a2
nestorivanmo <nestor.martinez.98@hotmail.com>
2022-03-21 21:01:30 -0600

    lecture 3

---
+++ 9-String-Processing-And-Computational-Geometry/Lecture-3-Suffix-Array-And-Suffix-Tree/1-Suffix-Trie/suffix_trie.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO() {
+    string file = __FILE__;
+    file = string(file.begin(), file.end()-3);
+    string in_file = file+"in";
+    string out_file = file+"out";
+    freopen(in_file.c_str(), "r", stdin);
+    freopen(out_file.c_str(), "w", stdout);
+}
+
+// References: https://cp-algorithms.com/string/aho_corasick.html
+
+const int K = 26;
+
+struct Vertex {
+    int next[K];
+    bool leaf = false;
+
+    Vertex() {
+        fill(begin(next), end(next), -1);
+    }
+};
+
+vector<Vertex> trie(1);
+
+void add_string(string const& s) {
+    int v = 0;
+    for (char ch : s) {
+        int c = ch - 'a';
+        if (trie[v].next[c] == -1) {
+            trie[v].next[c] = trie.size();
+            trie.emplace_back();
+        }
+        v = trie[v].next[c];
+    }
+    trie[v].leaf = true;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    if(getenv("LOCAL")){setIO();}
+
+    string s;
+    cin >> s;
+
+    add_string(s);
+
+    cout << trie.size() << endl;
+
+    return 0;
+}
